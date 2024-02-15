1ebea3aac5d139bf454ead6f20dd399015c0be62
gh-johnny <jmfurtadoromero@gmail.com>
2024-02-15 18:00:43 -0300

    chore: zod + resolver implemented

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,19 +1,33 @@
 import { Play } from "phosphor-react"
 import { useForm } from "react-hook-form"
-
+import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from 'zod'
 import { CountDownContainer, FormContainer, HomeContainer, MinutesAmountInput, Separator, StartCountDownButton, TaskInput } from "./styles"
+
+const newCycleFormValidationSchema = z.object({
+    task: z.string().min(1, 'Please specify the task'),
+    minutesAmount: z.number().min(5, 'Minimum is 5min').max(60, 'Max is 60min'),
+})
+
+type TNewCycleFormValidationSchema = z.infer<typeof newCycleFormValidationSchema>
 
 function Home() {
 
-    const { register, handleSubmit, watch } = useForm()
+    const { register, handleSubmit, watch, } = useForm<TNewCycleFormValidationSchema>({
+        resolver: zodResolver(newCycleFormValidationSchema),
+        defaultValues: {
+            task: '',
+            minutesAmount: 0,
+        }
+    })
 
-    const handleCreateNewCycle = (data: any) => {
+    const handleCreateNewCycle = (data: TNewCycleFormValidationSchema) => {
         console.log(data)
     }
 
-    const task = watch('task')
-    const isSubmitDisabled = !!!task
 
+    const task = watch('task')
+    const isSubmitDisabled = !task
 
     return (
         <HomeContainer>
