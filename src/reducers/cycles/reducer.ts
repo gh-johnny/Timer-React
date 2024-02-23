a9ba7c20c161dd3d68736ca3ea9112ac88bab98a
gh-johnny <jmfurtadoromero@gmail.com>
2024-02-23 11:23:21 -0300

    refactor: immer (mutability lib) added to reducer logic

--- src/reducers/cycles/reducer.ts
+++ src/reducers/cycles/reducer.ts
@@ -1,3 +1,4 @@
+import { produce } from "immer"
 import { EActionTypes } from "./actions"
 
 export type TCycle = {
@@ -17,23 +18,31 @@ interface ICyclesState {
 function cyclesReducer(state: ICyclesState, action: { type: EActionTypes, payload?: any }) {
     switch (action.type) {
         case EActionTypes.ADD_NEW_CYCLE:
-            return {
-                ...state,
-                cycles: [...state.cycles, action.payload.newCycle],
-                activeCycleId: action.payload.newCycle.id
-            }
-        case EActionTypes.INTERRUPT_CYCLE:
-            return {
-                ...state,
-                cycles: state.cycles.map(cycle => cycle.id === state.activeCycleId ? { ...cycle, interruptedDate: new Date() } : cycle),
-                activeCycleId: undefined,
-            }
-        case EActionTypes.MARK_AS_FINISHED:
-            return {
-                ...state,
-                cycles: state.cycles.map(cycle => cycle.id === state.activeCycleId ? { ...cycle, finishedDate: new Date() } : cycle),
-                activeCycleId: undefined,
-            }
+            return produce(state, draft => {
+                draft.cycles.push(action.payload.newCycle)
+                draft.activeCycleId = action.payload.newCycle.id
+            })
+
+        case EActionTypes.INTERRUPT_CYCLE: {
+            const currentCycleIndex = state.cycles.findIndex(cycle => cycle.id === state.activeCycleId)
+            const indexExists = currentCycleIndex > 0
+            if (!indexExists) return state
+
+            return produce(state, draft => {
+                draft.cycles[currentCycleIndex].interruptedDate = new Date()
+                draft.activeCycleId = undefined
+            })
+        }
+        case EActionTypes.MARK_AS_FINISHED: {
+            const currentCycleIndex = state.cycles.findIndex(cycle => cycle.id === state.activeCycleId)
+            const indexExists = currentCycleIndex > 0
+            if (!indexExists) return state
+
+            return produce(state, draft => {
+                draft.cycles[currentCycleIndex].finishedDate = new Date()
+                draft.activeCycleId = undefined
+            })
+        }
         default:
             return state
     }
